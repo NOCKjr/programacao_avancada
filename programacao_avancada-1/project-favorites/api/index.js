f1c914bd750af4ff552666a6bac2b77a4f0f16c7
NOCKjr <eliascruznocjr123@gmail.com>
2022-12-05 20:20:10 -0500

    aula 05/12

---
+++ programacao_avancada-1/project-favorites/api/index.js
@@ -0,0 +1,19 @@
+const data = require('./urls.json')
+const URL = require('url')
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+http.createServer((req, res) => {
+
+    const {name, url, del } = URL.parse(req.url, true).query
+    
+    if(!name || !url)
+       return res.end ('show')
+
+    if(del)
+        return res.end ('delete')
+
+    return res.end('create')  
+}).listen(3000, () => console.log('API is running...'))
+
--- programacao_avancada-1/project-favorites/public/scripts.js
+++ programacao_avancada-1/project-favorites/public/scripts.js
@@ -2,6 +2,8 @@ const ul = document.querySelector('ul')
 const input = document.querySelector('input')
 const form = document.querySelector('form')
 
+
+/*
 // Função que carrega o conteúdo da API.
 async function load() {
     // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
@@ -12,13 +14,28 @@ async function load() {
 }
 
 load()
+*/
 
-function addElement() {
+function addElement({ name, url }) {
+    const li = document.createElement('li')
+    const a = document.createElement("a")
+    const trash = document.createElement("span")
 
+    a.href = url
+    a.innerHTML = name
+    a.target = "_blank"
+
+    trash.innerHTML = "x"
+    trash.onclick = () => removeElement(trash)
+
+    li.append(a)
+    li.append(trash)
+    ul.append(li)
 }
 
-function removeElement() {
-
+function removeElement(el) {
+    if (confirm('Tem certeza que deseja deletar?'))
+        el.parentNode.remove()
 }
 
 form.addEventListener('submit', (event) => {
